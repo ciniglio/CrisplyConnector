3f76e29e8e9af842a47c4f5141853685d778cac9
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-08-10 19:27:30 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrisplyConnector::Application.config.secret_token = 'e77b1d670dce0e18d64dadaebffba2c7ada7136034f70aa6a28283d80377412535f306d73d987c5d330356630008c99033029f4685b5f4957a556a5953a97435'

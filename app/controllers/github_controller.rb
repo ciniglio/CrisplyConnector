9fe21eda83667a541ea93053bab6379d3fdddaf1
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-08-11 02:52:06 -0700

    Added github handler

---
+++ app/controllers/github_controller.rb
@@ -0,0 +1,17 @@
+class GithubController < ApplicationController
+  def post
+    ca = CrisplyApi.new subdomain: params[:subdomain], 
+                        apikey: params[:apikey]
+
+
+    params[:commits].each do |commit|
+      ca.author = commit[:author][:email]
+      ca.guid = commit[:id]
+      ca.text = commit[:message]
+      ca.date = commit[:timestamp]
+      ca.post_activity
+    end
+    
+    render nothing: true, status: 200
+  end
+end

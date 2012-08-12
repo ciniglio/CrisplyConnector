0392d9e5dbd3a9e05d71cc78983c0e038b43a004
Alejandro Ciniglio <mail@alejandrociniglio.com>
2012-08-12 02:04:05 -0700

    Cleaned up unnecessary logging.

--- app/controllers/pivotal_controller.rb
+++ app/controllers/pivotal_controller.rb
@@ -4,12 +4,6 @@ class PivotalController < ApplicationController
     ca = myCA.new subdomain: params[:subdomain],
                              apikey: params[:apikey]
 
-    puts "HI LOG"
-    puts params
-    puts "B LOG"
-    puts params[:activity]
-    puts "C LOG"
-
     activity = params[:activity]
     ca.author = activity[:author]
     ca.guid = activity[:id]
@@ -17,8 +11,6 @@ class PivotalController < ApplicationController
     ca.date = activity[:occurred_at]
     ca.post_activity
     
-    
-    render nothing: true, status: 200
-    
+    render nothing: true, status: 200    
   end
 end
